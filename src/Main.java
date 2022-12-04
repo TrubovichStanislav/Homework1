b9908c9da6672e28104cad034f1448f1e1c0ea2d
stas <nestas67@mail.ru>
2022-12-04 11:59:28 +0300

    Homework1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет, мирю");
+        // перевая часть задания
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("Первая часть задания");
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        System.out.println();
+
+
+        // вторая часть задания
+        dog +=4;
+        cat +=4;
+        paper +=4;
+        System.out.println("Вторая часть задания");
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        System.out.println();
+
+
+        // третья часть задания
+        dog -= 3.5;
+        cat -=1.6;
+        paper -= 7639;
+        System.out.println("Третья часть задания");
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
     }
 }
